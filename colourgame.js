e5700d0860a9eb7f21d5b106153f4d897f132257
Jonathon McCallum <jonmccallum@Jons-MacBook-Pro-2.local>
2020-09-06 14:50:23 +0100

    Initial Commit

---
+++ colourgame.js
@@ -0,0 +1,112 @@
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square")
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+init();
+
+function init(){
+    setupModeButtons();
+    setupSquares();
+    reset();
+}
+
+function setupSquares() {
+    for(var i = 0; i < squares.length; i++){
+        squares[i].addEventListener("click", function(){
+            var clickedColor = this.style.backgroundColor;
+            if(clickedColor === pickedColor){
+                messageDisplay.textContent = "Correct!";
+                resetButton.textContent = "Play Again?";
+                changeColors(clickedColor);
+                h1.style.backgroundColor = clickedColor;
+                var correct = new Audio("correct.wav");
+                correct.play();
+            }
+            else {
+                this.style.backgroundColor = "#232323";
+                messageDisplay.textContent = "Try Again!";
+                var inCorrect = new Audio("incorrect.wav");
+                inCorrect.play();
+            }
+        })
+    }
+}
+
+function setupModeButtons() {
+    for(var i = 0; i < modeButtons.length; i++){
+        modeButtons[i].addEventListener("click", function(){
+            modeButtons[0].classList.remove("selected");
+            modeButtons[1].classList.remove("selected");
+            this.classList.add("selected");
+            this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+            reset();
+        })
+    }
+}
+
+
+function reset (){
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
+    h1.style.backgroundColor = "steelblue";
+    resetButton.textContent = "New Colours";
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.display = "block";
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else {
+            squares[i].style.display = "none";
+        }
+
+    };
+}
+
+resetButton.addEventListener("click", function(){
+    var shuffle = new Audio("shuffle.wav");
+    shuffle.play();
+    reset();
+    setTimeout(function(){ reset(); }, 1000);
+    setTimeout(function(){ reset(); }, 2000);
+    setTimeout(function(){ reset(); }, 2700);
+})
+
+//changes all squares to correct color
+function changeColors(color){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+};
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+};
+
+function generateRandomColors(num){
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    var red =  Math.floor(Math.random() * 256)
+    var green =  Math.floor(Math.random() * 256)
+    var blue =  Math.floor(Math.random() * 256)
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
+
+//to add
+//add a score system
+//add a timer /timeout system - no answer = -1 point
+//add a pause button
